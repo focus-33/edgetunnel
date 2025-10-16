81c55f99a44533ce8bfb517f74c883c623ba9452
focus-33 <90539843+focus-33@users.noreply.github.com>
2025-10-16 17:40:07 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 ﻿
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '4167ac9b-b481-4bf3-b421-3a12bd464ab4';
 let proxyIP = '';
-let DNS64Server = '';
+let DNS64Server = 'dns64.fm2.ztvi.org';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
@@ -7195,4 +7195,5 @@ function config_Html(token = "test", proxyhost = "") {
 </html>`;
 
     return html;
+
 }
